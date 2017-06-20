ce94ff9911ba7d9d5f4f5d07a9e24f13d2022030
Jia Shen Boon <jiashenboon@Jias-MacBook-Air.local>
2017-06-20 08:13:43 -0700

    Add script to install Chrome

---
+++ rdpToUbuntu/install-chrome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Chrome
+# https://askubuntu.com/questions/510056/how-to-install-google-chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update -y
+sudo apt-get install google-chrome-stable -y
